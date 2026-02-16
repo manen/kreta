419dfc507217ab4e6f421f4bf3b949d585cb27bb
manen <49951816+manen@users.noreply.github.com>
2026-02-16 21:27:33 +0100

    `absence-analyzer`: change html stat design a lil

--- absence-analyzer/src/html_stats.rs
+++ absence-analyzer/src/html_stats.rs
@@ -45,21 +45,21 @@ pub fn by_excuse_type_unsorted<'a>(
 	data: impl Iterator<Item = (&'a ExcuseType, &'a AbsenceDetails)>,
 ) -> String {
 	let cols = data.map(|(ty, details)| {
-		let col_height = (details.hours * 0.6) as i32;
+		let col_height = details.hours * 0.6;
 		let col_width = 5.0;
 		let col_color = hash_to_color(ty);
 		let col_text = std::format_args!("{:.1}", details.hours);
 
 		let col = format!(
 			"
-			<div style=\"display: flex; justify-content: center; flex-direction: column;\">
-				<div style=\"display: flex; justify-content: center; color: black; padding: 0.5rem;\">
+			<div style=\"display: flex; align-items: center; flex-direction: column;\">
+				<div style=\"display: flex; justify-content: center; color: black; margin: 0.5rem; margin-bottom: 0.3rem;\">
 					{ty}
 				</div>
+				<div style=\"display: flex; justify-content: center; color: black; margin: 0.2rem; font-weight: bold;\">
+					{col_text} óra
+				</div>
 				<div style=\"height: {col_height}em; width: {col_width}em; background-color: {col_color};\">
-					<div style=\"display: flex; justify-content: center; color: white; padding: 0.2rem; font-weight: bold\">
-						{col_text}
-					</div>
 				</div>
 			</div>"
 		);

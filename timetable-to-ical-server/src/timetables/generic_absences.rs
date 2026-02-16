c10bb4019d2adc64ee31fa544e92540603f6c71c
manen <49951816+manen@users.noreply.github.com>
2026-02-16 22:59:44 +0100

    compatablility: ensure `timetable-to-ical-server` (so everything else too) can be compiled without any of the default features

--- timetable-to-ical-server/src/timetables/generic_absences.rs
+++ timetable-to-ical-server/src/timetables/generic_absences.rs
@@ -10,27 +10,37 @@ pub async fn generic_absences(
 	credentials: &Credentials,
 	clients: web::Data<Mutex<Clients>>,
 ) -> anyhow::Result<Html> {
-	let absences_raw = {
-		let client = {
-			let mut clients = clients.lock().await;
-			clients.client(credentials).await?
+	#[cfg(feature = "absence-analyzer")]
+	{
+		let absences_raw = {
+			let client = {
+				let mut clients = clients.lock().await;
+				clients.client(credentials).await?
+			};
+			let client = client.lock().await;
+
+			let absences = absence_analyzer::retreive::fetch_absences(&client)
+				.await
+				.with_context(|| {
+					format!(
+						"failed to fetch every absence for user {}",
+						credentials.username()
+					)
+				})?;
+
+			absences
 		};
-		let client = client.lock().await;
 
-		let absences = absence_analyzer::retreive::fetch_absences(&client)
-			.await
-			.with_context(|| {
-				format!(
-					"failed to fetch every absence for user {}",
-					credentials.username()
-				)
-			})?;
+		let absences = absence_analyzer::absences_by_excuse_type(absences_raw.iter());
+		let html = absence_analyzer::html_stats::html_stats(&absences);
 
-		absences
-	};
+		Ok(Html::new(html))
+	}
 
-	let absences = absence_analyzer::absences_by_excuse_type(absences_raw.iter());
-	let html = absence_analyzer::html_stats::html_stats(&absences);
-
-	Ok(Html::new(html))
+	#[cfg(not(feature = "absence-analyzer"))]
+	{
+		Ok(Html::new(
+			"absence-analyzer wasn't enabled on this server at compile time",
+		))
+	}
 }

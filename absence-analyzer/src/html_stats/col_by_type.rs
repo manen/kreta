60136e4cafdf95c0b44736902a6c92e912d027f1
manen <49951816+manen@users.noreply.github.com>
2026-02-16 21:30:20 +0100

    `absence-analyzer`: change dir structure

--- absence-analyzer/src/html_stats/col_by_type.rs
+++ absence-analyzer/src/html_stats/col_by_type.rs
@@ -1,33 +1,5 @@
-use std::{
-	fmt::Display,
-	hash::{DefaultHasher, Hash, Hasher},
-};
-
+use super::*;
 use crate::{AbsenceDetails, AbsencesByExcuse, ExcuseType};
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
-pub struct Color {
-	r: u8,
-	g: u8,
-	b: u8,
-}
-impl Display for Color {
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		write!(f, "rgb({}, {}, {})", self.r, self.g, self.b)
-	}
-}
-
-pub fn hash_to_color<T: Hash>(value: &T) -> Color {
-	let mut hasher = DefaultHasher::new();
-	value.hash(&mut hasher);
-	let hash = hasher.finish(); // u64
-
-	Color {
-		r: (hash >> 24) as u8,
-		g: (hash >> 16) as u8,
-		b: (hash >> 8) as u8,
-	}
-}
 
 pub fn by_excuse_type(data: &AbsencesByExcuse) -> String {
 	let mut sorted = data.absences.iter().collect::<Vec<_>>();
---
+++ absence-analyzer/src/html_stats/mod.rs
@@ -0,0 +1,31 @@
+use std::{
+	fmt::Display,
+	hash::{DefaultHasher, Hash, Hasher},
+};
+
+mod col_by_type;
+pub use col_by_type::*;
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+pub struct Color {
+	r: u8,
+	g: u8,
+	b: u8,
+}
+impl Display for Color {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "rgb({}, {}, {})", self.r, self.g, self.b)
+	}
+}
+
+pub fn hash_to_color<T: Hash>(value: &T) -> Color {
+	let mut hasher = DefaultHasher::new();
+	value.hash(&mut hasher);
+	let hash = hasher.finish(); // u64
+
+	Color {
+		r: (hash >> 24) as u8,
+		g: (hash >> 16) as u8,
+		b: (hash >> 8) as u8,
+	}
+}
